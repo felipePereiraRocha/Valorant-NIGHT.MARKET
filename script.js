12f2d68f591ab32aee8a3a81839ffc0bf02c641a
felipePereiraRocha <160747507+felipePereiraRocha@users.noreply.github.com>
2024-02-26 16:52:21 -0300

    NIGHT.MARKET V1.2
    - Added dynamic countdown

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-const day = Date([7, 9]);
-
-
-
+const myDate = new Date();
+var day = myDate.getDate();
 console.log(day);
+
+function Month() {
+    const myMonth = new Date();
+    var month = myMonth.getMonth();
+    if (month = 2){
+        console.log("It worked");
+    }
+}
+//DAYS LEFT//
+
+
+
+var daysLeft = 28 - day;
+var daysLeft= daysLeft.toString();
+
+let DAYS = document.getElementById('DAYSLEFT').innerHTML = daysLeft;
+DAYS.innerText = daysLeft;
+
+console.log(daysLeft);
