c7889e8ac1244ee5c2e40c933d41c990f1972f0e
felipePereiraRocha <160747507+felipePereiraRocha@users.noreply.github.com>
2024-02-28 14:36:25 -0300

    NIGHT.MARKET V1.21
    - Added a animation that plays upon loading the page.

--- script.js
+++ script.js
@@ -1,22 +1,32 @@
 const myDate = new Date();
 var day = myDate.getDate();
 console.log(day);
-
+//DYNAMIC MONTH COUNTDOWN(i will figure that one out eventualy)//
 function Month() {
     const myMonth = new Date();
     var month = myMonth.getMonth();
-    if (month = 2){
-        console.log("It worked");
-    }
+    console.log(getMonth());
+    if (month = 3){
+        var totalDays = 28;
+        return totalDays - 50;
+    
+    
+}
 }
 //DAYS LEFT//
 
-
-
 var daysLeft = 28 - day;
-var daysLeft= daysLeft.toString();
 
-let DAYS = document.getElementById('DAYSLEFT').innerHTML = daysLeft;
-DAYS.innerText = daysLeft;
+var DAYS = document.getElementById('DAYSLEFT').innerHTML = daysLeft;
 
 console.log(daysLeft);
+//CHECKS IF IT SHOULD DISPLAY DAY OR DAYS//
+
+if (daysLeft = 1){
+    var DAY = document.getElementById('span-days').innerHTML = " DAY";
+}else if (daysLeft <= 0){
+    var DAY = document.getElementById('span-days').innerHTML = " TODAY";
+    var DAYS = document.getElementById('DAYSLEFT').innerHTML = "";
+}else {
+    var DAY = document.getElementById('span-days').innerHTML = " DAYS";
+}
